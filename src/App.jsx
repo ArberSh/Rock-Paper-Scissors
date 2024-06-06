7e7294967c32c8a951a3788d1de6d14944506571
Arber Shaska <arbershaska2@gmail.com>
2024-06-06 22:02:52 +0200

    Added some Stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,78 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [number,setNumber] = useState()
-  const BotChoice = ["rock","paper","scissors"]
-  const [PlayerChoice,SetPlayerChoice] = useState()
-  const [PlayerDidAChoice,SetPlayerDidAChoice] = useState(false)
-  const [WhoWins,SetWhoWins] = useState("")
-  const [Clicked,SetClicked] = useState(0)
+  const [PlayerChoice, setPlayerChoice] = useState("");
+  const [number, setNumber] = useState();
+  const BotChoice = ["rock", "paper", "scissors"];
+  const [ArrayForBots, SetArrayForBots] = useState([]);
+  const [PlayerDidAChoice, SetPlayerDidAChoice] = useState(false);
+  const [WhoWins, SetWhoWins] = useState("");
+  const [GameNumber, SetGameNumber] = useState(0);
+  const [PlayerWon,SetPlayerWon] = useState(0)
+  const [BotWon,SetBotWon] = useState(0)
 
   useEffect(() => {
-    setNumber(Math.floor(Math.random() * 3)); 
-  }, [Clicked]);
+    setNumber(Math.floor(Math.random() * 3));
+    SetArrayForBots((array) => [...array, BotChoice[number]]);
+  }, [GameNumber]);
 
-  console.log(number)
-  console.log(BotChoice[number]) 
-  console.log(Clicked)
-  
-function PlayerChoose(e){
-  SetClicked(Clicked => Clicked + 1)
-  if(e.target.value === BotChoice[number]){
-      SetPlayerDidAChoice(true)
-      SetWhoWins("draw")
-    }
-    else if((e.target.value === "rock" && BotChoice[number] === "paper") || (e.target.value === "scissors" && BotChoice[number] === "rock") || (e.target.value === "paper" && BotChoice[number] === "scissors")){
-      SetPlayerDidAChoice(true)
-      SetWhoWins("Bot wins")
-    }
-    else{
-      SetPlayerDidAChoice(true)
-      SetWhoWins("Player wins")
+  console.log(ArrayForBots);
+  console.log(number);
+  console.log(BotChoice[number]);
+  console.log(PlayerDidAChoice);
+
+  function PlayerChoose(e) {
+    setPlayerChoice(e.target.value)
+    SetPlayerDidAChoice(true);
+    SetGameNumber(GameNumber => GameNumber + 1);
+    if (PlayerChoice === BotChoice[number]) {
+      SetWhoWins("draw");
+    } else if (
+      (PlayerChoice === "rock" && BotChoice[number] === "paper") ||
+      (PlayerChoice === "scissors" && BotChoice[number] === "rock") ||
+      (PlayerChoice === "paper" && BotChoice[number] === "scissors")
+    ) {
+      SetBotWon(Index => Index + 1)
+      SetWhoWins("Bot wins");
+    } else {
+      SetWhoWins("Player wins");
+      SetPlayerWon(Index => Index + 1)
     }
   }
 
-
   return (
     <>
-    {PlayerDidAChoice && 
-    <h1>Bot Choose:  {BotChoice[number]}</h1>
-    }
-      
-      <button className='m-1' value="rock" onClick={PlayerChoose}>Rock</button>
-      <button className='m-1' value="paper" onClick={PlayerChoose}>Paper</button>
-      <button className='m-1' value="Scissors" onClick={PlayerChoose}>Scissors</button>
-
-      <h1 className='text-lg'>{WhoWins}</h1>
+      <div className="md:flex flex-col justify-center items-center h-screen">
+        <div className="flex">
+          <div className="flex items-center flex-col bg-amber-400">
+            <h1>{PlayerWon}</h1>
+            <h1>Player Choose:</h1>
+            <h1>{PlayerChoice}</h1>
+          </div>
+          <div className="flex flex-col text-center bg-lime-500">
+            <h1>{BotWon}</h1>
+            <h1>Bot Choose: </h1>
+            {PlayerDidAChoice && (
+              <h1>{ArrayForBots[ArrayForBots.length - 1]}</h1>
+            )}
+          </div>
+        </div>
+        <div className="">
+          <button className="m-1" value="rock" onClick={PlayerChoose}>
+              Rock
+            </button>
+            <button className="m-1" value="paper" onClick={PlayerChoose}>
+              Paper
+            </button>
+            <button className="m-1" value="scissors" onClick={PlayerChoose}>
+              Scissors
+            </button>
+          <h1 className="text-center text-lg">{WhoWins}</h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
