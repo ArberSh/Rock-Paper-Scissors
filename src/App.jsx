e761c0832902bc93d73850fa7494b46e022b7dfc
Arber Shaska <arbershaska2@gmail.com>
2024-06-07 22:59:18 +0200

    Added Pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Rock from "./assets/Rock.png"
+import Paper from "./assets/Paper.png"
+import Scissors from "./assets/Scissors.png"
 
 function App() {
   const [PlayerChoice, setPlayerChoice] = useState("");
   const [number, setNumber] = useState();
   const BotChoice = ["rock", "paper", "scissors"];
-  const [ArrayForBots, SetArrayForBots] = useState([]);
   const [PlayerDidAChoice, SetPlayerDidAChoice] = useState(false);
   const [WhoWins, SetWhoWins] = useState("");
   const [GameNumber, SetGameNumber] = useState(0);
   const [PlayerWon,SetPlayerWon] = useState(0)
   const [BotWon,SetBotWon] = useState(0)
 
-  useEffect(() => {
-    setNumber(Math.floor(Math.random() * 3));
-    SetArrayForBots((array) => [...array, BotChoice[number]]);
-  }, [GameNumber]);
-
-  console.log(ArrayForBots);
   console.log(number);
   console.log(BotChoice[number]);
-  console.log(PlayerDidAChoice);
+  console.log(PlayerChoice);
 
-  function PlayerChoose(e) {
-    setPlayerChoice(e.target.value)
-    SetPlayerDidAChoice(true);
-    SetGameNumber(GameNumber => GameNumber + 1);
-    if (PlayerChoice === BotChoice[number]) {
+  useEffect(()=>{
+    if (number !== undefined && PlayerChoice !== ""){
+    const botChoice =BotChoice[number]
+    if (PlayerChoice === botChoice) {
       SetWhoWins("draw");
     } else if (
-      (PlayerChoice === "rock" && BotChoice[number] === "paper") ||
-      (PlayerChoice === "scissors" && BotChoice[number] === "rock") ||
-      (PlayerChoice === "paper" && BotChoice[number] === "scissors")
+      (PlayerChoice === "rock" && botChoice === "paper") ||
+      (PlayerChoice === "scissors" && botChoice === "rock") ||
+      (PlayerChoice === "paper" && botChoice === "scissors")
     ) {
       SetBotWon(Index => Index + 1)
       SetWhoWins("Bot wins");
@@ -39,6 +34,16 @@ function App() {
       SetWhoWins("Player wins");
       SetPlayerWon(Index => Index + 1)
     }
+  }
+  },[number, PlayerChoice])
+
+
+  function PlayerChoose(e) {
+    console.log(e.target.value + "player choooseeee")
+    setNumber(Math.floor(Math.random() * 3));
+    setPlayerChoice(e.target.value)
+    SetPlayerDidAChoice(true);
+    SetGameNumber(GameNumber => GameNumber + 1);
   }
 
   return (
@@ -54,19 +59,20 @@ function App() {
             <h1>{BotWon}</h1>
             <h1>Bot Choose: </h1>
             {PlayerDidAChoice && (
-              <h1>{ArrayForBots[ArrayForBots.length - 1]}</h1>
+              <h1>{BotChoice[number]}</h1>
             )}
           </div>
         </div>
         <div className="">
-          <button className="m-1" value="rock" onClick={PlayerChoose}>
-              Rock
+          <button className="m-2 w-36 p-2 border-gray-900 border-solid border-4" value="rock" onClick={PlayerChoose}>
+            
+              <img className="rotate-90 scale-x-[-1]" src={Rock} alt="" />
             </button>
-            <button className="m-1" value="paper" onClick={PlayerChoose}>
-              Paper
+            <button className="m-2 w-36 px-2 pb-2 pt-5 border-gray-900 border-solid border-4" value="paper" onClick={PlayerChoose}>
+              <img className="rotate-90 scale-x-[-1]" src={Paper} alt="" />
             </button>
-            <button className="m-1" value="scissors" onClick={PlayerChoose}>
-              Scissors
+            <button className="m-2 w-40 px-4 pt-0.5 pb-2 border-gray-900 border-solid border-4" value="scissors" onClick={PlayerChoose}>
+              <img className="rotate-90 scale-x-[-1]" src={Scissors} alt="" />
             </button>
           <h1 className="text-center text-lg">{WhoWins}</h1>
         </div>
